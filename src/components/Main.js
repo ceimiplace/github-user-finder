8572d7cd8b79a253c958729a606b2f1d9a57fcbc
ceimiplace <azamfiredenis@yahoo.com>
2022-12-29 13:16:36 +0200

    finished main header component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,20 @@
 export default function Main() {
   return (
     <main className="main-container">
-      <section>sdfr</section>
+      <header className="main-container__header">
+        <img
+          src="https://s3.amazonaws.com/media.eremedia.com/uploads/2013/05/23151721/Octocat.jpg"
+          alt="picture profile of searched user"
+          className="main-container__header__picture"
+        />
+        <section>
+          <p className="main-container__header_username">Name</p>
+          <a className="main-container__header_link" href="#">
+            Link to page
+          </a>
+          <p className="main-container__header_userjoined">When did he join</p>
+        </section>
+      </header>
     </main>
   );
 }
