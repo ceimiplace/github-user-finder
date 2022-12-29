65885280db23b99afd65a9a566310b79079aeb18
ceimiplace <azamfiredenis@yahoo.com>
2022-12-29 12:01:38 +0200

    added header STYLING

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import LightSwitch from "./LightSwitch";
 export default function Header({ children }) {
   return (
     <header className="header">
-      <h1>devfinder</h1>
+      <h1 className="header__heading">devfinder</h1>
       <LightSwitch isDark={false} />
     </header>
   );
--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -3,7 +3,7 @@ import lighticon from "../assets/icon-sun.svg";
 export default function LightSwitch({ isDark }) {
   return (
     <div className="light-switch">
-      <p className="light-switch__para">{isDark ? "Light" : "Dark"}</p>
+      <p className="light-switch__text">{isDark ? "Light" : "Dark"}</p>
       <img src={isDark ? lighticon : darkicon} />
     </div>
   );
