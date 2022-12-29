e28cb4113bae29193e3f134902455affd4632233
ceimiplace <azamfiredenis@yahoo.com>
2022-12-29 13:01:52 +0200

    started working on main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { getSuggestedQuery } from "@testing-library/react";
 import Input from "./components/Input";
 import Header from "./components/Header";
+import Main from "./components/Main";
 function App() {
   return (
     <div className="App">
       <div className="app__wrapper">
         <Header></Header>
         <Input />
+        <Main />
       </div>
     </div>
   );
