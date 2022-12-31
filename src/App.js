bbcac8cbcc2bbdf4dfae1187c2f3e2b498c155b9
ceimiplace <azamfiredenis@yahoo.com>
2022-12-31 14:44:50 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import { getSuggestedQuery } from "@testing-library/react";
 import Input from "./components/Input";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { useEffect, useState } from "react";
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    async function SearchUser(input) {
+      // fetch("https://api.github.com/users/" + input)
+      //   .then((resp) => resp.json())
+      //   .then((data) => {
+      //     console.log(data);
+      //     setUserData(data);
+      //   });
+      const octokit = new Octokit({
+        auth: "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",
+      });
+
+      const denis = await octokit.request("GET /users/{ceimiplace}", {
+        username: "ceimiplace",
+      });
+      console.log(denis);
+    }
+    if (userInput) {
+      SearchUser();
+    }
+  }, [userInput]);
+
   return (
     <div className="App">
       <div className="app__wrapper">
         <Header></Header>
-        <Input />
+        <Input changeMainState={setUserInput} />
         <Main />
       </div>
     </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import search from "../assets/icon-search.svg";
 
-export default function Input() {
+export default function Input({ changeMainState }) {
+  const [input, setInput] = useState("");
+  function formSubmit(e) {
+    e.preventDefault();
+    changeMainState(input);
+  }
   return (
-    <form className="input-container">
+    <form className="input-container" onSubmit={formSubmit}>
       <img src={search} />
       <input
         type="text"
         className="input-container__input"
         placeholder="Search Github Username...."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button className="input-container__search_button">Search</button>
     </form>
