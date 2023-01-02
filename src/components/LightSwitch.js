ef9e555bc18946bf0dad3315aefa600b070ab810
ceimiplace <azamfiredenis@yahoo.com>
2023-01-02 10:28:53 +0200

    added button for light switch

--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -4,13 +4,17 @@ export default function LightSwitch({ setDarkMode, darkMode }) {
   return (
     <div className="light-switch">
       <p className="light-switch__text">{darkMode ? "Light" : "Dark"}</p>
-
-      <img
-        className="light-switch-button"
-        alt="light mode switch"
+      <button
+        area-label="Dark mode switch"
+        className="dark-mode-button"
         onClick={() => setDarkMode(!darkMode)}
-        src={darkMode ? lighticon : darkicon}
-      />
+      >
+        <img
+          className="light-switch-button"
+          alt="light mode switch"
+          src={darkMode ? lighticon : darkicon}
+        />
+      </button>
     </div>
   );
 }
